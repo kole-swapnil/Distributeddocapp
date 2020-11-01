c21f5e35c382aded6705b70d0bd10537459c28cf
Swapnil Kole <swapnilkole7500@gmail.com>
2020-11-01 18:56:16 +0530

    component added

---
+++ client/src/Components/SharedComponent.js
@@ -0,0 +1,26 @@
+import Frame from 'react-frame-component';
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import '../App.css';
+import { render } from 'react-dom';
+const iframe = '<iframe height="600" style="width: 90%;" scrolling="no" title="fx." src="https://atiya1-hub.github.io/dicomimage" frameborder="no" allowtransparency="true" allowfullscreen="true"></iframe>'; 
+
+
+function Iframe(props) {
+    return (<div dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
+  }
+
+function SharedComp() {
+  return (
+    <div className="App">
+      <h1>OHIF VIEWER</h1>
+      <Iframe iframe={iframe} />,
+    </div>
+  );
+}
+
+
+
+
+export default SharedComp;
